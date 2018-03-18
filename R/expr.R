577f236fd8fc672417f2f59fe73698aef9088267
Yafei <lyuyafei@gmail.com>
2018-03-18 18:22:14 -0400

    005

--- R/expr.R
+++ R/expr.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset containing gene expression levels for rat brain and heart tissues. The expression intensity were measured using Affymetrix Rat Exon 1.0 ST Array. 100 differential genes are selected to construct example co-expression network for 2 tissues.
 #'
-#' @format A data list that containing 2 matrices
+#' @format A data list that containing 2 data matrices
 #' \describe{
 #'   \item{brain}{expression levels of 100 gene for brain tissue}
 #'   \item{heart}{expression levels of 100 gene for heart tissue}
